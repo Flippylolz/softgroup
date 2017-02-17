6ece7babf94dd35801b095b4bf8c4d39f2ff13b9
Flippylolz <assasin463@gmail.com>
2017-02-17 17:26:33 +0200

    deleted puts

--- brackets.rb
+++ brackets.rb
@@ -1,17 +1,18 @@
-require 'benchmark.rb'
 class String
   def valid?
     string = self.gsub(/[^\(\)\[\]\{\}\<\>]/, '')
     return true if string.empty?
     return false if string.size.odd?
     loop do
-      p s = string.gsub('()', '').gsub('[]', '').gsub('{}', '').gsub('<>', '')
+      s = string.gsub('()', '').gsub('[]', '').gsub('{}', '').gsub('<>', '')
       return true if s.empty?
       return false if s == string
       string = s
     end
   end
 end
+
+
 # testing
 # p '[ ({[]})( ) {<>} ]'.valid? # => true
 # p '[ ( text { more text { (even <)>more text) }} )]'.valid? # => false
